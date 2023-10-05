bec94a7611de27a94d6a23a580eae9be9d5f5be8
trandai2609 <trandai2609@gmail.com>
2023-10-05 22:17:16 +0700

    update code

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aebe808f8c0514e86bbe1d5db9b39a09-1166777816.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
